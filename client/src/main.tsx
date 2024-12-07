043c01473a727a7af148748b815da448dc650f86
JonashGana <jonashtiglao@gmail.com>
2024-12-07 23:07:11 +0800

    setup Router provider and QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Root from './route/root'
+import HomePage from './pages/HomePage'
+import MoviesPage from './pages/MoviesPage'
 import './styles/index.css'
-import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: '/movies',
+        element: <MoviesPage />
+      }
+    ]
+  }
+])
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300000,
+      gcTime: 10 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
   </StrictMode>,
 )
