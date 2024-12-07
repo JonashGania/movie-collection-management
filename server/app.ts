6ebeb312e0b1eac2481b941f2b91c11ff3620bbc
JonashGana <jonashtiglao@gmail.com>
2024-12-07 23:08:00 +0800

    added cors

--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import moviesRouter from './routes/movies.js';
 import genresRouter from './routes/genres.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.use('/api', moviesRouter);
 app.use('/api', genresRouter);
