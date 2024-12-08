b993018d9f13b89051fff15d01e851d7efdb1157
JonashGana <jonashtiglao@gmail.com>
2024-12-08 23:15:09 +0800

    fix get movies controller

--- server/controllers/moviesControllers.ts
+++ server/controllers/moviesControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { getMoviesPaginated } from "../db/queries.js"
+import { getMoviesPaginated, getMoviePoster } from "../db/queries.js"
 
 export const getAllMoviesPaginated = async(req: Request, res: Response) => {
     const page = parseInt(req.query.page as string, 10) || 1;
@@ -10,14 +10,27 @@ export const getAllMoviesPaginated = async(req: Request, res: Response) => {
     }
 
     try {
-        const allMovies = await getMoviesPaginated(page);
+        const movies = await getMoviesPaginated(page);
 
-        if (allMovies?.length === 0) {
+        if (!movies || movies.length === 0) {
             res.status(404).json({ error: "No movies found on this page" });
             return;
         }
 
-        res.status(200).json({page, allMovies});
+        const allMovies = await Promise.all(
+            movies.map(async (movie) => {
+                try {
+                    const posterUrl = await getMoviePoster(movie.id, movie.title);
+                    return {...movie, poster_url: posterUrl };
+                } catch (error) {
+                    console.error(`Failed to fetch poster for movie ID ${movie.id}`, error);
+                    return {...movie, poster_url: null}
+                }
+                
+            })
+        )
+
+        res.status(200).json({page, movies: allMovies});
     } catch (error) {
         console.error('Error in getAllMoviesPaginated controller', error);
         res.status(500).json({ error: "An error occured while fetching movies." });
