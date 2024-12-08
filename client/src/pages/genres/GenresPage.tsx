bbce782b74b47e3daeb23e3e6d94da5b3ff6e7f8
JonashGana <jonashtiglao@gmail.com>
2024-12-08 23:15:56 +0800

    initial commit

---
+++ client/src/pages/genres/GenresPage.tsx
@@ -0,0 +1,14 @@
+const GenresPage = () => {
+    return (
+        <section className="max-w-7xl mx-auto w-full px-4">
+            <div className="pt-12">
+                <div className="flex items-center">
+                    <div className="w-[5px] h-10 bg-cyan-300 rounded-lg"></div>
+                    <h1 className="font-bold text-4xl pl-4 text-white">Genres</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GenresPage
---
+++ client/src/pages/movies/MoviesPage.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query"
+import { useState } from "react"
+import { getAllMovies } from "../../api";
+import MovieWrapper from "./MovieWrapper";
+
+const MoviesPage = () => {
+    const [page, setPage] = useState(1);
+
+    const {data, isError, isLoading} = useQuery({
+        queryKey: ['movies', page],
+        queryFn: () => getAllMovies(String(page))
+    })
+
+    if (isLoading) {
+        return (
+            <div>Loading</div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div>Error</div>
+        )
+    }
+
+    return (
+        <section className="max-w-7xl mx-auto px-4 pb-12">
+            <div className="pt-12">
+                <div className="flex items-center">
+                    <div className="w-[5px] h-10 bg-cyan-300 rounded-lg"></div>
+                    <h1 className="font-bold text-4xl pl-4 text-white">All Movies</h1>
+                </div>
+                {data && (
+                    <MovieWrapper movies={data}/>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default MoviesPage
