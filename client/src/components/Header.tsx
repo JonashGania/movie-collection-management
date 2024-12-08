d53efb3083ba241bafa3f8d3ef84b87ef3622984
JonashGana <jonashtiglao@gmail.com>
2024-12-08 23:11:43 +0800

    map thru links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom"
 
 const Header = () => {
+    const links = [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'Movies',
+            link: '/movies'
+        },
+        {
+            name: 'Genres',
+            link: '/genres'
+        },
+    ];
+
     return (
         <header className="w-full">
             <nav className="max-w-7xl h-[100px] mx-auto px-4 flex items-center justify-between">
                 <h1 className="text-cyan-300 text-xl font-semibold">Movie Collection</h1>
                 <ul className="flex items-center gap-4 text-zinc-200 text-lg font-medium">
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/movies"}>Movies</Link>
-                    <Link to={"/genres"}>Genres</Link>
+                    {links.map((item) => (
+                        <Link to={item.link}>{item.name}</Link>
+                    ))}
                 </ul>
             </nav>
         </header>
