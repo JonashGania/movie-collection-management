7909e0a210d724ed8d2c7504595d799582cecde2
JonashGana <jonashtiglao@gmail.com>
2024-12-09 22:37:35 +0800

    fetch all genres using useQuery

--- client/src/pages/genres/GenresPage.tsx
+++ client/src/pages/genres/GenresPage.tsx
@@ -1,11 +1,26 @@
+import { useQuery } from "@tanstack/react-query"
+import { getAllGenres } from "@/api"
+import GenresWrapper from "./GenresWrapper"
+
 const GenresPage = () => {
+
+    const {data, isError, isLoading} = useQuery({
+        queryKey: ['genres'],
+        queryFn: () => getAllGenres() 
+    })
+
+    console.log(data);
+
     return (
-        <section className="max-w-7xl mx-auto w-full px-4">
+        <section className="max-w-7xl mx-auto w-full px-4 pb-12">
             <div className="pt-12">
                 <div className="flex items-center">
                     <div className="w-[5px] h-10 bg-cyan-300 rounded-lg"></div>
                     <h1 className="font-bold text-4xl pl-4 text-white">Genres</h1>
                 </div>
+                {data && (
+                    <GenresWrapper genres={data}/>
+                )}
             </div>
         </section>
     )
