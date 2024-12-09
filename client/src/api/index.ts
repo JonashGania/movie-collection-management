2f326150408e9ea79e4ba5e690aec6b7f9d554d3
JonashGana <jonashtiglao@gmail.com>
2024-12-09 22:36:47 +0800

    fetch all genres

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -7,3 +7,8 @@ export const getAllMovies = async (page: string): Promise<MoviesPaginated> => {
     const response = await axios.get(`${API_BASE_URL}/api/movies?page=` + page);
     return response.data
 }
+
+export const getAllGenres = async () => {
+    const response = await axios.get(`${API_BASE_URL}/api/genres`);
+    return response.data
+}
