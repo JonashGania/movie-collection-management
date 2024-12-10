48259ac0af1f9ebf9687bf34ca6568b2d2bb4f43
JonashGana <jonashtiglao@gmail.com>
2024-12-10 22:50:34 +0800

    initial commit

---
+++ client/src/components/MovieWrapper.tsx
@@ -0,0 +1,19 @@
+import { Movies } from "../types"
+import MovieCard from "./MovieCard"
+
+interface MovieWrapperProps {
+    movies: { movies: Movies[]}
+}
+
+const MovieWrapper = ({ movies }: MovieWrapperProps) => {
+    return (
+        <div className="pt-8">
+            <div className="movie-wrapper w-full">
+                {movies.movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie}/>
+                ))}
+            </div>
+        </div>
+    )
+}   
+export default MovieWrapper
