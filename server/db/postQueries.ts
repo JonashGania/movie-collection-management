e6e24826d50abac4f1e330b45ccedb70161adf85
JonashGana <jonashtiglao@gmail.com>
2024-12-13 23:38:02 +0800

    created insert queries

---
+++ server/db/postQueries.ts
@@ -0,0 +1,85 @@
+import { pool } from "./pool.js"
+
+export const createMovieQuery = async (
+    title: string,
+    release_date: string,
+    rating: number,
+    description: string,
+    duration: number,
+    slug: string,
+    poster_url: Promise<string>,
+    genres: string[],
+    actors: string[],
+    directors: string[],
+) => {
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+
+        const movieResult = await client.query(`
+            INSERT INTO movies (title, release_date, description, duration, rating, slug)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id`,
+            [title, release_date, description, duration, rating, slug]
+        )
+    
+        const movieId = movieResult.rows[0].id;
+
+        await client.query(`
+            INSERT INTO movie_images (movie_id, poster_url)
+            VALUES ($1, $2)`,
+            [movieId, poster_url]
+        )
+    
+   
+        await client.query(`
+            INSERT INTO movie_genres (movie_id, genre_id) 
+            SELECT $1, unnest($2::uuid[])`,
+            [movieId, genres]
+        )
+
+        const directorIds = (await Promise.all(
+            directors.map(directorName => 
+                client.query(`
+                    INSERT INTO directors (director_name)
+                    VALUES ($1)
+                    ON CONFLICT (director_name) DO UPDATE SET director_name = EXCLUDED.director_name
+                    RETURNING id`,
+                    [directorName]
+                )
+            )
+        )).map(res => res.rows[0].id);
+
+        await client.query(`
+            INSERT INTO movie_directors(movie_id, director_id)
+            SELECT $1, unnest($2::uuid[])`,
+            [movieId, directorIds]
+        )
+
+        const actorIds = (await Promise.all(
+            actors.map(actorName => 
+                client.query(`
+                    INSERT INTO actors (actor_name)
+                    VALUES ($1)
+                    ON CONFLICT (actor_name) DO UPDATE SET actor_name = EXCLUDED.actor_name
+                    RETURNING id`,
+                    [actorName]
+                )
+            )
+        )).map(res => res.rows[0].id);
+
+        await client.query(`
+            INSERT INTO movie_actors (movie_id, actor_id)
+            SELECT $1, unnest($2::uuid[])`,
+            [movieId, actorIds]
+        )
+
+        await client.query('COMMIT')
+    } catch (error) {
+        await client.query('ROLLBACK')
+        console.error('Error creating movie', error)
+        throw error;
+    } finally {
+        client.release();
+    }
+}
