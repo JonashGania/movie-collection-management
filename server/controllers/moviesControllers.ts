ef535eb10fb0f70bb751a0788e507fd87b7c799b
JonashGana <jonashtiglao@gmail.com>
2024-12-13 23:39:53 +0800

    setup controller for movie post request

--- server/controllers/moviesControllers.ts
+++ server/controllers/moviesControllers.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express"
 import { getMoviesPaginated, getMoviePoster } from "../db/getQueries.js"
+import { createMovieQuery } from "../db/postQueries.js";
+import { movieTitleSlug } from "../utils/generateSlug.js";
+import { fetchMoviePoster } from "../utils/fetchMoviePoster.js";
 
 export const getAllMoviesPaginated = async(req: Request, res: Response) => {
     const page = parseInt(req.query.page as string, 10) || 1;
@@ -41,3 +44,38 @@ export const getAllMoviesPaginated = async(req: Request, res: Response) => {
     }
 }
 
+export const createMovies = async (req: Request, res: Response, next: NextFunction) => {
+    const {  
+        title,
+        release_date,
+        rating,
+        duration,
+        description,
+        genres,
+        actors,
+        directors
+    } = req.body
+
+    try {
+        const year = new Date(release_date).getFullYear().toString();
+        const slug = movieTitleSlug(title, year)
+        const posterUrl = await fetchMoviePoster(title)
+
+        if (!title || !release_date || !rating || !duration || !description) {
+            res.status(400).send("Missing required fileds: titles, release_date, rating, duration, description.")
+            return
+        }
+
+        if (!Array.isArray(genres) || !Array.isArray(actors) || !Array.isArray(directors)) {
+            res.status(400).send("Invalid data: genres, actors, directors must be an arrays.")
+            return
+        }
+
+        await createMovieQuery(title, release_date, rating, description, duration, slug, posterUrl, genres, actors, directors)
+    
+        res.status(201).send("Movie created successfully.");
+    } catch (error) {
+        console.error('Error creating movie', error)
+        res.status(500).send("An error occured while creating the movie")
+    }
+}
