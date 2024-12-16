2ec730c22dedac83204a3b21e922fb2af5abc064
JonashGana <jonashtiglao@gmail.com>
2024-12-16 23:12:52 +0800

    added watchlist link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
+
 
 const Header = () => {
     const links = [
@@ -20,17 +21,26 @@ const Header = () => {
         <header className="w-full">
             <nav className="max-w-7xl h-[100px] mx-auto px-4 flex items-center justify-between">
                 <h1 className="text-cyan-300 text-xl font-semibold">Movie Collection</h1>
-                <ul className="flex items-center gap-8 text-zinc-200 text-lg font-medium">
-                    {links.map((item, index) => (
-                        <NavLink 
-                            to={item.link} 
-                            key={index} 
-                            className="text-white font-light text-lg"
-                        >
-                            {item.name}
-                        </NavLink>
-                    ))}
-                </ul>
+                <div className="flex items-center">
+                    <ul className="flex items-center gap-8 text-zinc-200 text-lg font-medium pr-3">
+                        {links.map((item, index) => (
+                            <NavLink 
+                                to={item.link} 
+                                key={index} 
+                                className="text-white font-light text-lg"
+                            >
+                                {item.name}
+                            </NavLink>
+                        ))}
+                    </ul>
+                    <div className="pl-3 border-l-2 border-zinc-600">
+                        <Link to={'/watchlist'} className="flex items-center gap-2">
+                            <img src="/square-plus.svg" alt="icon" />
+                           <span className="text-gray-200 font-semibold">Watchlist</span>
+                        </Link>
+                    </div>
+                </div>
+                
             </nav>
         </header>
     )
