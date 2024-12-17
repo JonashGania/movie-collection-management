65920906cd8151c963d98dccbda6b17c176c8e25
JonashGana <jonashtiglao@gmail.com>
2024-12-17 22:42:43 +0800

    created GET route /movies/:movieId

--- server/routes/movies.ts
+++ server/routes/movies.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllMoviesPaginated, createMovies } from '../controllers/moviesControllers.js';
+import { getAllMoviesPaginated, createMovies, getMovieDetails } from '../controllers/moviesControllers.js';
 
 const moviesRouter = express.Router();
 
-moviesRouter.get('/movies', getAllMoviesPaginated)
+moviesRouter.get('/movies', getAllMoviesPaginated);
+moviesRouter.get('/movies/:movieId', getMovieDetails);
 moviesRouter.post('/movies', createMovies);
 
 export default moviesRouter;
