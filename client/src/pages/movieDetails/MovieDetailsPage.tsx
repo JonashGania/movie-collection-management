b48afe75e9159ef552c1fcd93b8acb0ffb8f11e0
JonashGana <jonashtiglao@gmail.com>
2024-12-18 23:22:31 +0800

    updated

--- client/src/pages/movieDetails/MovieDetailsPage.tsx
+++ client/src/pages/movieDetails/MovieDetailsPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import { getMovieDetails } from "@/api";
 import { useQuery } from "@tanstack/react-query";
-import { CalendarDays, Clock, Star } from "lucide-react";
+import { CalendarDays, Clock, Star, PlusIcon } from "lucide-react";
 import { formatYear } from "@/utils/formatYear";
 
 const MovieDetailsPage = () => {
@@ -18,10 +18,10 @@ const MovieDetailsPage = () => {
     }
 
     return (
-        <section className="max-w-7xl mx-auto w-full pt-12 px-4">
+        <section className="max-w-7xl mx-auto w-full pt-12 px-4 ">
             <div className="flex justify-between gap-4">
                 <div>
-                    <h1 className="text-white text-5xl pb-2">{data.title}</h1>
+                    <h1 className="text-white text-4xl pb-2">{data.title}</h1>
                     <ul className="flex items-center">
                         <li className="flex items-center gap-2 pr-2">
                             <CalendarDays className="h-4 w-4" color="#67e8f9"/>
@@ -49,21 +49,48 @@ const MovieDetailsPage = () => {
 
             <div className="flex flex-wrap w-full pt-4 gap-8">
                 <div className="w-[25%] relative">
-                    <img src={`${data.poster_url}`} alt={`${data.title} poster`} className="w-full max-h-[420px]"/>
+                    <img 
+                        src={`${data.poster_url}`} 
+                        alt={`${data.title} poster`} 
+                        className="w-full max-h-[420px] rounded-md"
+                    />
                 </div>
-                <div className="flex-1">
-                    <div className="flex items-center gap-4 pb-2">
-                        {data.genres.map((genre, index) => (
-                            <div key={index} className="text-white px-4 py-1 border border-zinc-600 rounded-2xl">{genre}</div>
-                        ))}
-                    </div>
-                    <p className="text-white">{data.description}</p>
+                <div className="flex-1 flex flex-col justify-between">
                     <div>
-
+                        <div className="flex items-center gap-4 pb-2">
+                            {data.genres.map((genre, index) => (
+                                <div key={index} className="text-cyan-500 font-medium px-4 py-1 bg-[rgba(90,115,146,0.2)] rounded-2xl">{genre}</div>
+                            ))}
+                        </div>
+                        <p className="text-white pb-3">{data.description}</p>
+                        <div className="flex items-center py-3 border border-l-transparent border-r-transparent border-t-zinc-600 border-b-zinc-600">
+                            <span className="text-white font-semibold">Director</span>
+                            <ul className="flex items-center">
+                                {data.directors.map((director, index) => (
+                                    <li key={index} className="pl-2 text-yellow-400 font-medium ">{director}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="flex items-center py-3 border border-l-transparent border-r-transparent border-transparent border-b-zinc-600">
+                            <span className="text-white font-semibold">Stars</span>
+                            <ul className="flex items-center">
+                                {data.actors.map((actor, index) => (
+                                    <li key={index} className="pl-2 text-yellow-400 font-medium ">{actor}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <button className="flex items-center gap-3 bg-cyan-700 hover:bg-cyan-600 px-6 py-2 rounded-3xl">
+                            <PlusIcon className="h-5 w-5" color="#fff"/>
+                            <span className="text-white">Add to Watchlist</span>
+                        </button>
+                        <button className="px-6 py-1 bg-white rounded-3xl text-lg font-medium">Edit</button>
+                        <button className="px-6 py-1 bg-red-500 rounded-3xl text-lg text-white font-medium">Delete</button>
                     </div>
                 </div>
             </div>
-        </section>
+        </ section>
     )
 }
 
