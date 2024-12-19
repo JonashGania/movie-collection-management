ccfcd752f457677076805e691845c2b0ad5f72f1
JonashGana <jonashtiglao@gmail.com>
2024-12-19 22:57:47 +0800

    initial commit

---
+++ client/src/components/DeleteButton.tsx
@@ -0,0 +1,18 @@
+import { Dialog, DialogTrigger } from "./ui/dialog"
+import DeleteMovieModal from "./DeleteMovieModal"
+
+const DeleteButton = ({ movieId }: {movieId: string | undefined}) => {
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <button className="px-6 py-1 bg-red-600 hover:bg-red-500 rounded-3xl text-lg text-white font-medium"
+                >
+                    Delete
+                </button>
+            </DialogTrigger>
+            <DeleteMovieModal movieId={movieId}/>
+        </Dialog>
+    )
+}
+
+export default DeleteButton
