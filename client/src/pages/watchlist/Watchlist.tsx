96938a8b252842a978da5236e413e569df591017
JonashGana <jonashtiglao@gmail.com>
2024-12-21 23:23:39 +0800

    initial commit

---
+++ client/src/pages/watchlist/Watchlist.tsx
@@ -0,0 +1,12 @@
+const Watchlist = () => {
+    return (
+        <section className="max-w-7xl mx-auto w-full pt-12 px-4 ">
+            <div>
+                <h1 className="text-white text-3xl font-medium mb-4">Your Watchlist</h1>
+                <p className="max-w-[700px] text-zinc-400">Your watchlists are saved here to track the movies you want to watch. You can sort your Watchlist by the rating, date, runtime and title.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Watchlist
