7dcded14a020a5918a5e5c6ee053ff1d68081501
JonashGana <jonashtiglao@gmail.com>
2024-12-22 22:00:42 +0800

    added routes

--- server/routes/watchlist.ts
+++ server/routes/watchlist.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addToWatchlist, getAllWatchlist } from '../controllers/watchlistControllers.js';
+import { addToWatchlist, getAllWatchlist, removeFromWatchlist } from '../controllers/watchlistControllers.js';
 
 const watchlistRouter = express.Router();
 
 watchlistRouter.get('/watchlist', getAllWatchlist);
 watchlistRouter.post('/watchlist/:movieId', addToWatchlist);
+watchlistRouter.delete('/watchlist/:movieId', removeFromWatchlist);
 
 export default watchlistRouter
