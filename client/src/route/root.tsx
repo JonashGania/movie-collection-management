6998808a72573f080018f2d337b465882725c2d4
JonashGana <jonashtiglao@gmail.com>
2024-12-22 21:59:03 +0800

    wrapped WatchlistProvider

--- client/src/route/root.tsx
+++ client/src/route/root.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
+import { WatchlistProvider } from "@/context/WatchlistContext"
 
 const Root = () => {
     return(
-        <div className="font-inter bg-[#0b0c10] w-full min-h-screen">
-            <Header />
-            <Outlet />
+        <div className="font-inter bg-[#0b0c10] w-full min-h-screen flex flex-col">
+            <WatchlistProvider>
+                <Header />
+                <Outlet />
+            </WatchlistProvider>
         </div>
     )
 }
