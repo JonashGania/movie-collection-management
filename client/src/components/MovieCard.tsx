5c8f9d75668a0ea9d3b9ef30358044be58694e1d
JonashGana <jonashtiglao@gmail.com>
2024-12-22 21:56:23 +0800

    implement add and remove movie from watchlist

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,19 +1,37 @@
 import { Movies } from "@/types"
 import { formatYear } from "@/utils/formatYear";
 import { Link } from "react-router-dom";
+import { useWatchlist } from "@/context/WatchlistContext";
+import { PlusIcon, Check } from "lucide-react";
+import {OrbitProgress} from 'react-loading-indicators'
 
 interface MovieCardProps {
     movie: Movies;
 }
 
 const MovieCard = ({ movie }: MovieCardProps) => {
+    const { watchlist, addMovieToWatchlist, removeMovieFromWatchlist, isAdding } = useWatchlist();
+
+    const isInWatchlist = watchlist.some((item) => item.id === movie.id);
+    const isAddingMovie = isAdding(movie.id);
+
+    const handleWatchlistClick = () => {
+        if (isInWatchlist) {
+            removeMovieFromWatchlist(movie.id)
+        } else {
+            addMovieToWatchlist(movie.id)
+        }
+    }
+
     return (
-        <Link 
-            to={`/movies/${movie.slug}`} 
+        <div 
             key={movie.id} 
             className="movie-card rounded-md bg-[#1f2833] flex flex-col pb-4 max-w-[235px] w-full"
         >
-            <div className="relative w-full">
+            <Link 
+                to={`/movies/${movie.slug}`} 
+                className="relative w-full"
+            >
                 {movie.poster_url ? (
                     <div className="movie-image">
                         <img 
@@ -26,7 +44,8 @@ const MovieCard = ({ movie }: MovieCardProps) => {
                 ) : (
                     <div>No image available</div>
                 )}
-            </div>
+                <div className="absolute w-full h-full top-0 left-0 hover:bg-[rgba(41,41,41,0.1)]"></div>
+            </Link>
             <div className="flex justify-between gap-2 pt-4 px-2">
                 <div className="flex items-center gap-1">
                     <img src="/star-icon.svg" alt="star icon" />
@@ -39,12 +58,25 @@ const MovieCard = ({ movie }: MovieCardProps) => {
             </div>
             <h5 className="pt-2 px-2 text-gray-100 font-semibold text-lg leading-5 flex-grow">{movie.title}</h5>
             <div className="px-2 pt-4">
-                <button className="bg-[rgba(90,115,146,0.2)] w-full px-4 py-2 rounded-3xl hover:bg-[rgba(90,115,146,0.3)] duration-200 flex items-center gap-2 justify-center">
-                    <img src="/plus-sign.svg" alt="plus sign" />
-                    <span className="text-cyan-500 font-medium">Watchlist</span>
+                <button
+                    onClick={handleWatchlistClick} 
+                    className="bg-[rgba(90,115,146,0.2)] w-full px-4 py-2 rounded-3xl hover:bg-[rgba(90,115,146,0.3)] duration-200 flex items-center gap-2 justify-center"
+                >
+                    {isAddingMovie ? (
+                        <OrbitProgress color="#06b6d4" style={{ fontSize: "4px" }}/>
+                    ) : (
+                        <>
+                            {isInWatchlist ? (
+                                <Check className="h-6 w-5" color="#06b6d4"/>
+                            ) : (
+                                <PlusIcon className="h-6 w-5" color="#06b6d4"/>
+                            )}
+                            <span className="text-cyan-500 font-medium">Watchlist</span>
+                        </>
+                    )}
                 </button>
             </div>
-        </Link>
+        </div>
     )
 }
 
