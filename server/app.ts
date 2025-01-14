14c611effe1483df549dd2f61eb914964dae5052
JonashGana <jonashtiglao@gmail.com>
2025-01-14 23:33:04 +0800

    added passport middlewares

--- server/app.ts
+++ server/app.ts
@@ -1,23 +1,41 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-import moviesRouter from './routes/movies.js';
-import genresRouter from './routes/genres.js';
-import watchlistRouter from './routes/watchlist.js';
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
+import passport from 'passport';
 import cors from 'cors';
+import router from './routes/index.js';
+import { pool } from './config/dbConnect.js';
+import * as dotenv from 'dotenv';
+import './config/local-strategy.js'
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 8000;
 
+const pgSession = connectPgSimple(session);
+
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', moviesRouter);
-app.use('/api', genresRouter);
-app.use('/api', watchlistRouter);
+app.use(session({
+    store: new pgSession({
+        pool: pool,
+        tableName: 'session'
+    }),
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24     // 1 day
+    }
+}))
+
+app.use(passport.session());
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
