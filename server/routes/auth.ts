29333ed775578e92206c235374591457c68cfa76
JonashGana <jonashtiglao@gmail.com>
2025-01-15 23:55:43 +0800

    auth route

---
+++ server/routes/auth.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import passport from "passport";
+import { createUser } from '../controllers/authController.js';
+import { User } from '../types/index.js';
+import { checkAuthentication } from '../middlewares/authMiddleware.js';
+
+const authRouter = express.Router();
+
+authRouter.post('/sign-up', createUser);
+
+authRouter.post('/login', (req, res, next) => {
+    passport.authenticate("local", (err: Error, user: User, info: any) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                status: 401,
+                message: info ? info.message : 'Login failed'
+            })
+        }
+
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            return res.json({ message: "Login successful" });
+        })
+    })(req, res, next);
+})
+
+authRouter.get('/check-auth', checkAuthentication)
+
+export default authRouter
