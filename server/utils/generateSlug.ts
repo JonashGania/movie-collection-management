4d030d9e53058cede17fb4b7c4afa87281229237
JonashGana <jonashtiglao@gmail.com>
2025-01-17 23:43:46 +0800

    refactor function

--- server/utils/generateSlug.ts
+++ server/utils/generateSlug.ts
@@ -1,10 +1,13 @@
-import slug from 'slug'
+import { v4 as uuidV4 } from 'uuid';
 
-export const movieTitleSlug = (title: string, year: string) => {
-    return slug(`${title}-${year}`, { 
-        lower: true, 
-        remove: /[*+~.()'"!:@]/g 
-    })
+export const generateMovieSlug = (title: string, year: string) => {
+    const sanitizedTitle =  encodeURIComponent(title.trim().toLowerCase().replace(/:/g, '')).replace(/%20/g, '-');
+    const releaseYear = new Date(year).getFullYear().toString();
+    const firstLast = title[0].toLowerCase() + title[title.length - 1].toLowerCase();
+    const uuid = uuidV4();
+    const last5chars = uuid.slice(-5);
+
+    return `${sanitizedTitle}-${releaseYear}-${firstLast}-${last5chars}`
 }
 
 
